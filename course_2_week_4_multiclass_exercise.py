a882c4ac12016191064ffd5c90de92237455160e
Dima Budiak <dmitry.budyak@gmail.com>
2025-10-22 10:31:08 +0300

    Squashed 'python/coursera-ml/' content from commit 97f42fc
    git-subtree-dir: python/coursera-ml
    git-subtree-split: 97f42fc20361c400e98d96d72c798250c10fdee5

---
+++ course_2_week_4_multiclass_exercise.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# ATTENTION: Please do not alter any of the provided code in the exercise. Only add your own code where indicated
+# ATTENTION: Please do not add or remove any cells in the exercise. The grader will check specific cells based on the cell position.
+# ATTENTION: Please use the provided epoch values when training.
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+
+# In[ ]:
+
+
+def get_data(filename):
+    # You will need to write code that will read the file passed
+    # into this function. The first line contains the column headers
+    # so you should ignore it
+    # Each successive line contians 785 comma separated values between 0 and 255
+    # The first value is the label
+    # The rest are the pixel values for that picture
+    # The function will return 2 np.array types. One with all the labels
+    # One with all the images
+    #
+    # Tips:
+    # If you read a full line (as 'row') then row[0] has the label
+    # and row[1:785] has the 784 pixel values
+    # Take a look at np.array_split to turn the 784 pixels into 28x28
+    # You are reading in strings, but need the values to be floats
+    # Check out np.array().astype for a conversion
+    with open(filename) as training_file:
+        # Your code starts here
+        my_arr = np.loadtxt(filename, delimiter=',', skiprows=1)
+        labels = my_arr[:, 0].astype('int')
+        images = my_arr[:, 1:]
+        images = images.astype('float').reshape(images.shape[0], 28, 28)
+        my_arr = None
+        # Your code ends here
+    return images, labels
+
+
+path_sign_mnist_train = "data/sign_mnist_train.csv"
+path_sign_mnist_test = "data/sign_mnist_test.csv"
+training_images, training_labels = get_data(path_sign_mnist_train)
+testing_images, testing_labels = get_data(path_sign_mnist_test)
+
+# Keep these
+print(training_images.shape)
+print(training_labels.shape)
+print(testing_images.shape)
+print(testing_labels.shape)
+
+# Their output should be:
+# (27455, 28, 28)
+# (27455,)
+# (7172, 28, 28)
+# (7172,)
+
+
+# In[ ]:
+
+
+# In this section you will have to add another dimension to the data
+# So, for example, if your array is (10000, 28, 28)
+# You will need to make it (10000, 28, 28, 1)
+# Hint: np.expand_dims
+
+training_images = np.expand_dims(training_images, axis=3)
+testing_images = np.expand_dims(testing_images, axis=3)
+
+# Create an ImageDataGenerator and do Image Augmentation
+train_datagen = ImageDataGenerator(rescale=1. / 255.,
+                                   rotation_range=5,
+                                   width_shift_range=0.1,
+                                   height_shift_range=0.1,
+                                   shear_range=0.1,
+                                   zoom_range=0.1,
+                                   horizontal_flip=True)
+validation_datagen = ImageDataGenerator(
+    rescale=1.0 / 1.)
+
+# Keep These
+print(training_images.shape)
+print(testing_images.shape)
+
+# Their output should be:
+# (27455, 28, 28, 1)
+# (7172, 28, 28, 1)
+
+
+# In[ ]:
+
+
+# Define the model
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(25, activation='softmax')]
+)
+# Use no more than 2 Conv2D and 2 MaxPooling2D
+
+# Compile Model.
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Train the Model
+history = model.fit_generator(
+    generator=train_datagen.flow(training_images, training_labels),
+    epochs=2,
+    steps_per_epoch=len(training_images) / 100,
+    validation_data=validation_datagen.flow(testing_images, testing_labels),
+    verbose=1,
+    validation_steps=len(testing_images) / 100)
+
+model.evaluate(testing_images, testing_labels, verbose=0)
+
+# In[ ]:
+
+
+# Plot the chart for accuracy and loss on both training and validation
+import matplotlib.pyplot as plt
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'r', label='Training accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+
+plt.plot(epochs, loss, 'r', label='Training Loss')
+plt.plot(epochs, val_loss, 'b', label='Validation Loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
